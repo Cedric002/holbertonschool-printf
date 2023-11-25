403469fd72f4036fc7750f625bcc5fcf0c400012
Cedric002 <cedgnleblanc@gmail.com>
2023-11-25 07:59:24 -0800

    Ajout du fichier des fonctions supplémentaires

---
+++ ExtrasFunctions.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/*
+ * int formatUnsignedDecimal(va_list *args)
+ * {
+ * }
+ *
+ * int formatOctal(va_list *args)
+ * {
+ * }
+ *
+ * int formatUnsignedHexa(va_list *args)
+ * {
+ * }
+ *
+ * int formatAddress(va_list *args)
+ * {
+ * }
+ *
+ * int formatUnknown(va_list *args)
+ * {
+ * }
+ */
